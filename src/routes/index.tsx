71d82ce222f33df7302070b549632489c21f2ba7
fernandezjorgecontacto-cmyk <fernandezjorgecontacto@gmail.com>
2026-09-10 16:06:15 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -404,7 +404,7 @@ function Index() {
               style={{ border: "1px solid var(--gold)", opacity: 0.5 }}
             />
             <img
-              src="/src/assets/963a8d69-8f87-4265-a355-f61f311fbfb0.JPG"
+              src="/src/assets/fotomalcom.jpg"
               alt="Fotografía del escribano"
               className="relative w-full rounded-sm object-cover shadow-[0_30px_80px_-30px_oklch(0.24_0.05_258_/_0.35)]"
           />
